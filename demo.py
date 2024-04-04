38b851319e039fad92fd21bd4007f83955249d9c
Guillermo <gyanezfeliu@gmail.com>
2024-04-04 13:31:59 +0100

    improved demo file

--- demo.py
+++ demo.py
@@ -4,5 +4,11 @@ import napari
 from skimage.io import imread
 
 fname = '/Users/guillermo/Downloads/10x_1.0x_pLPT20&41_single_TiTweez_1_MMStack_Pos0.ome.tif'
-viewer = napari.view_image(imread(fname'), channel_axis=3)
+im = imread(fname)[:,:,:,1:]
+viewer = napari.view_image(
+	im, 
+	channel_axis=3,
+	name=['Phase', 'YFP', 'CFP'],
+	colormap=['gray','green','blue']
+	)
 
